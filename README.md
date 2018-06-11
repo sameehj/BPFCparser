214ccb30baeada3700d1f36ed95e6d7e77df50d0
Sameeh Jubran <sjubran@redhat.com>
2018-06-11 16:33:54 +0300

    Adding README.md parse_dump.sh
    Signed-off-by: Sameeh Jubran <sjubran@redhat.com>

---
+++ README.md
@@ -0,0 +1,40 @@
+## Instruction encoding
+
+An eBPF program is a sequence of 64-bit instructions. This project assumes each
+instruction is encoded in host byte order, but the byte order is not relevant
+to this spec.
+
+All eBPF instructions have the same basic encoding:
+
+    msb                                                        lsb
+    +------------------------+----------------+----+----+--------+
+    |immediate               |offset          |src |dst |opcode  |
+    +------------------------+----------------+----+----+--------+
+
+From least significant to most significant bit:
+
+ - 8 bit opcode
+ - 4 bit destination register (dst)
+ - 4 bit source register (src)
+ - 16 bit offset
+ - 32 bit immediate (imm)
+
+Most instructions do not use all of these fields. Unused fields should be
+zeroed.
+
+
+## Object file generation:
+Usually compiling the code for bpf code can be done by using the following command:
+
+clang -I path/to/iproute2/include -O2 -emit-llvm -c bpf_program.c -o - | llc -march=bpf -filetype=obj -o bpf_program.o
+
+## C-style array generation:
+The parse_dump script takes as an input a compiled object file and obtionally a section name and outputs a c-array style which can be used for initializing an array of the "struct bpf_insn" structure.
+
+struct bpf_insn {
+	__u8	code;		/* opcode */
+	__u8	dst_reg:4;	/* dest register */
+	__u8	src_reg:4;	/* source register */
+	__s16	off;		/* signed offset */
+	__s32	imm;		/* signed immediate constant */
+};
---
+++ parse_dump.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -s|--section)
+    SECTION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -o|--object-file)
+    OBJECTFILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -h|--help)
+    HELPMENU=true
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+
+if [[ -v HELPMENU ]] || ! [[ -v OBJECTFILE ]]; then
+    printf "This script can be used to create a c-array in 'struct bpf_insn' compatible format out of a bpf object file. The script uses llvm-objdump for dumping the 64 bit bpf command in hex fromat and awk to align them in comma separated format according to the following sizing:\n\
+* 8 bit opcode\n\
+* 4 bit destination register (dst)\n\
+* 4 bit source register (src)\n\
+* 16 bit offset\n\
+* 32 bit immediate (imm)\n\
+The parameters to the script that should be provided are section name and object file name using the following flags:\n\
+* -s|--section\n\
+* -o|--object-file\n"
+    exit
+fi
+
+if [[ -v SECTION ]]; then
+    SECTION="--section $SECTION"
+fi
+
+llvm-objdump -S --disassemble-all --print-imm-hex $SECTION $OBJECTFILE  | awk '{n=split($0,a,"\t"); if(n==3){split(a[2],b," "); print b[1]","substr(b[2],1,1)","substr(b[2],2,1)","b[3]b[4]","b[5]b[6]b[7]b[8]} next;}'
